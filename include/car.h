b06821099751d647d372b5da171a747e2b2a5460
bohushpalina <palinabohush@gmail.com>
2026-02-26 18:49:59 +0300

    Added comments to car.h

--- include/car.h
+++ include/car.h
@@ -1,5 +1,12 @@
+/* Author: Bohush Polina group 11a */
 #ifndef CAR_H
 #define CAR_H
+
+/*
+ * This header file acts as an interface for the car processing module.
+ * It defines the data structures and provides function prototypes
+ * required to read, display, and filter car data based on its age.
+ */
 
 #include <stdio.h>
 #define MAX_STR_LEN	64
