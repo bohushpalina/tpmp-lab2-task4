8ddd917844ab740c297519e83681562258e8f235
bohushpalina <palinabohush@gmail.com>
2026-02-26 18:47:51 +0300

    Added comments to main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+/* Author: Bohush Polina group 11a */
+
+/*
+ * The main client program that utilizes the car processing module.
+ * It defines the file names and the current year, then calls
+ * the module's interface function to process the data.
+ */
+
 #include <car.h>
 
 int main(void)
